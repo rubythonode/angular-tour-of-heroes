dd6f02e09e7e4380b156d433a52689f5e9a5b3f5
HyunSeob <sytte1017@gmail.com>
2017-01-02 10:14:59 +0900

    Initial commit

---
+++ app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {Routes, RouterModule} from '@angular/router';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {NgModule} from '@angular/core';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'heroes', component: HeroesComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,38 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import {AppComponent} from './app.component';
+import {HeroService} from './hero.service';
+import {DashboardComponent} from './dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HeroSearchComponent} from './hero-search.component';
+import {HeroSearchService} from './hero-search.service';
+
+import './rxjs-extensions';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    HeroSearchComponent
+  ],
+  providers: [ HeroService, HeroSearchService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
